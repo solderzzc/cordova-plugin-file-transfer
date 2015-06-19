d8a4cc2d1987378cf0fb616c704115aeeedf5ab4
solderzzc <solderzzc@gmail.com>
2015-06-18 21:27:55 -0700

    Update FileTransferBCS.java

--- src/android/FileTransferBCS.java
+++ src/android/FileTransferBCS.java
@@ -695,13 +695,14 @@ public class FileTransferBCS extends CordovaPlugin {
         }
         
         // TODO: refactor to also allow resources & content:
+        /*
         if (!isLocalTransfer) {
             Log.w(LOG_TAG, "Source URL is not in white list: '" + source + "'");
             JSONObject error = createFileTransferError(CONNECTION_ERR, source, target, null, 401, null);
             callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.IO_EXCEPTION, error));
             return;
         }
-
+        */
         
         final RequestContext context = new RequestContext(source, target, callbackContext);
         synchronized (activeRequests) {
